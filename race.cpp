11e52daa5e6613196ccfe4b62baf80463273c800
alexanderbell-ctrl <alexander.bell@bsu.edu>
2026-02-06 21:34:24 +0000

    started race

---
+++ race.cpp
@@ -0,0 +1,12 @@
+#include "race.h"
+#include "horse.h"
+
+void Race::start() {
+	bool keepGoing = true;
+	while(keepGoing) {
+		for(int i = 0; i < NUM_HORSES; i++) {
+			
+		} //end for
+	} //end while
+} //end start
+
