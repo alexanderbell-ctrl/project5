3ecdbcac00da028104eb7972d8525acd47c93890
alexanderbell-ctrl <alexander.bell@bsu.edu>
2026-02-06 20:05:14 +0000

    updated to test horse; 'too few arguments' error

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "horse.h"
+
+void testHorse(int index, int track_length);
 
 int main() {
 	std::cout << "OOP Horse Race" << std::endl;
+
+	testHorse();
+
 	return 0;
+}
+
+void testHorse() {
+	Horse h;
+	h.Horse_init(1, 15);
+	h.printLane();
 }
--- makefile
+++ makefile
@@ -1,5 +1,11 @@
-horseRace: main.cpp
-	g++ main.cpp -o horseRace
+horseRace: main.o horse.o
+	g++ main.o horse.o -o horseRace
+
+main.o: main.cpp horse.h
+	g++ -c main.cpp
+
+horse.o: horse.h horse.cpp
+	g++ -c horse.cpp
 
 clean:
 	rm horseRace
